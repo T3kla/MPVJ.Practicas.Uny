256ddbbd2bbeff610cdd90f3cea12b739157aa59
T3kla <manuillo94@gmail.com>
2022-05-30 21:16:20 +0200

    Bolos Lmao

---
+++ Subject15/Assets/Scripts/BoloDetector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoloDetector : MonoBehaviour
+{
+
+    [SerializeField] private Game Game = null;
+
+    private List<GameObject> Bolos = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider Col)
+    {
+        if (Col.gameObject.tag == "Bolo")
+            if(!Bolos.Contains(Col.gameObject))
+            {
+                Bolos.Add(Col.gameObject);
+                Game?.AddScore(1);
+            }
+    }
+}
---
+++ Subject15/Assets/Scripts/Game.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    [Header("Game")]
+    [SerializeField] private Transform BoloPrefab = null;
+    [SerializeField] private Transform BolaPrefab = null;
+    [SerializeField] private List<Transform> BoloPositions = new List<Transform>();
+    [SerializeField] private Transform BolaPosition = null;
+    [SerializeField] private float BolaSpeed = 0.0f;
+
+    [Header("UI")]
+    [SerializeField] private TMP_Text ScoreNumber = null;
+
+    private Transform Bola = null;
+    private Rigidbody BolaRB = null;
+
+    private int Score = 0;
+
+    private void Awake()
+    {
+        // Spawn bolos
+        foreach (var pos in BoloPositions)
+            Instantiate(BoloPrefab, pos.position, Quaternion.identity);
+
+        // Spawn bola
+        Bola = Instantiate(BolaPrefab, BolaPosition.position, BolaPosition.rotation);
+        BolaRB = Bola.GetComponent<Rigidbody>();
+
+        // Set score
+        ScoreNumber.text = Score.ToString();
+    }
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        // If player press space, add force to bola
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+           BolaRB.AddForce(Vector3.forward * BolaSpeed, ForceMode.Impulse);
+           BolaRB.AddTorque(Vector3.right * BolaSpeed, ForceMode.Impulse);
+        }
+    }
+
+    public void AddScore(int score)
+    {
+        Score += score;
+        ScoreNumber.text = Score.ToString();
+    }
+
+}
