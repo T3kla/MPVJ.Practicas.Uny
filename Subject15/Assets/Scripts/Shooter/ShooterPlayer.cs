9733c5828de3e4e5d1c7d0fc46ba62e28f0f4d42
T3kla <manuillo94@gmail.com>
2022-06-06 19:31:40 +0200

    a

---
+++ Subject15/Assets/Scripts/Shooter/ShooterPlayer.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class ShooterPlayer : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 10.0f;
+
+
+    private void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+}
